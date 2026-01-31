aac84d64c86a0ac5635ac6ca068632d75c1b0790
Krish <Krish02679@gmail.com>
2026-01-31 21:54:00 +0545

    Framework for quiz app added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Quiz App</h1>
+            <p>Test your knowledge with 10 multiple choice questions.</p>
+        </div>
+    );
+}
+
+export default Home;
